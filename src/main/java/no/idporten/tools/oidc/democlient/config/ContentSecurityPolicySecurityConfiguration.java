6be4830e6be87a090405668d2d9e33e2999b741c
Sigvald Høyheim <sigvald.hoyheim@digdir.no>
2023-07-27 11:21:01 +0200

    gjer csp header konfigurerbar via profil

--- src/main/java/no/idporten/tools/oidc/democlient/config/ContentSecurityPolicySecurityConfiguration.java
+++ src/main/java/no/idporten/tools/oidc/democlient/config/ContentSecurityPolicySecurityConfiguration.java
@@ -5,17 +5,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @Configuration
 public class ContentSecurityPolicySecurityConfiguration {
+
+    @Value("${oidc-demo-client.csp-header}")
+    private String cspHeader;
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.headers()
             .xssProtection()
             .and()
-            .contentSecurityPolicy("style-src 'self' cdn.jsdelivr.net; script-src 'self' cdn.jsdelivr.net; form-action 'self' idporten:7070")
+            .contentSecurityPolicy(cspHeader)
             .and()
             .referrerPolicy()
             .and()
