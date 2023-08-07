e3e3a4a874a08799de5a2f0740b37743046e9e90
Sigvald Høyheim <114908757+shoyheim-dd@users.noreply.github.com>
2023-08-07 08:23:34 +0200

    ID-3097: Oppdatere headere og content mtp sikkerhet (#195)
    * legg til headers og flytt inline js til eiga fil for å støtte csp
    
    * gjer csp header konfigurerbar via profil
    
    * Legg til støtte for testing med csrf

---
+++ src/main/java/no/idporten/tools/oidc/democlient/config/ContentSecurityPolicySecurityConfiguration.java
@@ -0,0 +1,31 @@
+package no.idporten.tools.oidc.democlient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+@Configuration
+public class ContentSecurityPolicySecurityConfiguration {
+
+    @Value("${oidc-demo-client.csp-header}")
+    private String cspHeader;
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers()
+            .xssProtection()
+            .and()
+            .contentSecurityPolicy(cspHeader)
+            .and()
+            .referrerPolicy()
+            .and()
+            .permissionsPolicy();
+        return http.build();
+    }
+}
