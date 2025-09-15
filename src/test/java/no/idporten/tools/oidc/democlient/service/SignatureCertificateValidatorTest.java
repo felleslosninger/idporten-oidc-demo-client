e52ea654c064175a3676ca5902e94533078396b5
Nils-Jarle Haugen <nils-jarle.haugen@digdir.no>
2025-09-15 16:00:48 +0200

    tests: cleanup imports

--- src/test/java/no/idporten/tools/oidc/democlient/service/SignatureCertificateValidatorTest.java
+++ src/test/java/no/idporten/tools/oidc/democlient/service/SignatureCertificateValidatorTest.java
@@ -21,7 +21,6 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 import static no.idporten.tools.oidc.democlient.TestDataUtils.*;
